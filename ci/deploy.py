95789042eece01be86502852fe6f5c28c5722b0d
Carlos Duarte <cduarte@lulobank.com>
2023-09-30 17:56:12 -0500

    Print entries in deploy job

--- ci/deploy.py
+++ ci/deploy.py
@@ -10,13 +10,10 @@ async def main():
     # initialize Dagger client
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         
-        # create a cache volume for Maven downloads
-        maven_cache = client.cache_volume("gradle-cache")
-
         # get reference to source code directory
-        source = client.host().directory(".", exclude=["ci", ".venv"])
+        entries = await client.host().directory(".", exclude=["ci", ".venv"]).entries()
 
-        print(source)
+        print(entries)
 
 
 anyio.run(main)
